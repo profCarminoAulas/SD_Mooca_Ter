09fb90a95abcf1c93a4d28c8c59207402b965485
josegomessaojudas <111508682+josegomessaojudas@users.noreply.github.com>
2022-10-06 06:21:55 -0300

    final aula 30 08

--- Aula_09_27/microservicos/barramento-de-eventos/index.js
+++ Aula_09_27/microservicos/barramento-de-eventos/index.js
@@ -14,7 +14,9 @@ app.post('/eventos', (req, res) => {
     //envia o evento para o microsserviço de observações
     axios.post('http://localhost:5000/eventos', evento);
     //envia o evento para o microsserviço de consulta
-    axios.post('http://localhost:6000/eventos', evento)
+    axios.post('http://localhost:6000/eventos', evento);
+    //envia o evento para o microsservico de classificacao
+    axios.post("http://localhost:7000/eventos", evento);
     res.status(200).send({ msg: "ok" });
 });
 
---
+++ Aula_10_04/barramento-de-eventos/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//para enviar eventos para os demais microsserviços
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json()); 
+
+const eventos = [];
+
+app.post("/eventos", (req, res) => {
+  const evento = req.body;
+  eventos.push(evento);
+  //envia o evento para o microsserviço de lembretes
+  axios.post("http://localhost:4000/eventos", evento);
+  //envia o evento para o microsserviço de observações
+  axios.post("http://localhost:5000/eventos", evento);
+  //envia o evento para o microsserviço de consulta
+  axios.post("http://localhost:6000/eventos", evento);
+  //envia o evento para o microsservico de classificacao
+  axios.post("http://localhost:7000/eventos", evento);
+  res.status(200).send({ msg: "ok" });
+});
+
+app.get("/eventos", (req, res) => {
+  res.send(eventos);
+});
+
+app.listen(10000, () => {
+  console.log("Barramento de eventos. Porta 10000.");
+});
